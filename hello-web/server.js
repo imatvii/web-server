87b2ec0674d51ed294ed433eb846f405d141e8eb
Ivan Matviichuk <imatviichuk@umass.edu>
2026-09-11 18:31:04 -0400

    first

---
+++ hello-web/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, web!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
+app.get('/projects', (req, res) => {
+  const projects = [
+    { name: 'Weather app', tag: 'javascript' },
+    { name: 'Portfolio site', tag: 'express' },
+    { name: 'Budget tracker', tag: 'python' },
+  ];
+  const tag = req.query.tag ?? null;
+  if (!tag) {
+    const resu = projects.map(p => p.name).join(' ');
+    return res.send(resu);
+  }
+  const filtered = projects.filter(p => p.tag === tag);
+  const resu = filtered.map(p => p.name).join(' ');
+  res.send(resu);
+});
